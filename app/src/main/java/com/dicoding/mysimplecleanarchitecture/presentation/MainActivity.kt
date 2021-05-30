22ecaf3af702f132bc7822a94424ceedb05b7a6c
Abdurrasyid Muhasibi <ice.syid@gmail.com>
2021-05-30 17:00:51 +0700

    test error message

--- app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/com/dicoding/mysimplecleanarchitecture/presentation/MainActivity.kt
@@ -10,20 +10,14 @@ import com.dicoding.mysimplecleanarchitecture.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
         val factory = MainViewModelFactory.getInstance()
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
-        viewModel.setName("Dicoding")
-        viewModel.message.observe(this, Observer {
-            binding.tvWelcome.text = it.welcomeMessage
-        })
+        val message = viewModel.getMessage("Dico")
+        tv_welcome.text = message.welcomeMessage
     }
 }
